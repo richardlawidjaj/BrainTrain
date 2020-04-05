abce8391d4864df4a0ecbad60be8a4270c300426
richardla <lawidjaja.richards@gmail.com>
2020-04-05 22:24:36 +0800

    Add Data Structure Challenge
    Add Data Structure Challenge

---
+++ BrainTrain/HackerRankTest/Data Structure/ArrayDsTest.cs
@@ -0,0 +1,20 @@
+﻿using BrainTrain.HackerRank.Data_Structure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRankTest.Data_Structure
+{
+    [TestClass]
+    public class ArrayDsTest
+    {
+        [TestMethod]
+        public void ArrayDsUnitTest()
+        {
+            int[] input= new int[] { 1, 2, 3, 4 };
+            var result = ArrayDs.ReverseArray(input);
+            Assert.IsTrue(result[0] == 4);
+        }
+    }
+}
